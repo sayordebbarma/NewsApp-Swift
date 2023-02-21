0a7bcba187a98ddf7c8f6ed84ac212aaef48014e
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-02-21 19:40:04 +0530

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Sayor Debbarma on 21/02/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
